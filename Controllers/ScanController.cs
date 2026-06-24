3bd13ca16cc4feb030e2ee12a694e95bbbffe7f2
RaymondSotelo <raymondsotelo75@gmail.com>
2026-06-24 20:19:51 +0800

    Initial commit of SmartScan project

---
+++ Controllers/ScanController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SmartScan.Models;
+// Make sure to include your Models namespace if ScannedItem is in the Models folder
+// using SmartScan.Models; 
+
+namespace SmartScan.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScanController : ControllerBase
+    {
+        [HttpPost("receive")]
+        public IActionResult ReceiveScanData([FromBody] List<ScannedItem> items)
+        {
+            if (items == null || items.Count == 0)
+            {
+                return BadRequest("No items were scanned.");
+            }
+
+            // Print the incoming items to the Visual Studio Output window to test the connection
+            foreach (var item in items)
+            {
+                System.Diagnostics.Debug.WriteLine($"Received: {item.ProductName} | Qty: {item.Quantity}");
+            }
+
+            return Ok(new { message = "Scan received successfully by ASP.NET!" });
+        }
+    }
+}
--- obj/Debug/net9.0/SmartScan.AssemblyInfo.cs
+++ obj/Debug/net9.0/SmartScan.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartScan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff421dc5379d554fa439fa8d2a4bd3ce24947e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ddbadfa83960375cecddb20c6e6a3dd03520d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartScan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartScan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
