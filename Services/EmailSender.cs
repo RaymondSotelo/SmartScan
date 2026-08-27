6264981d401b8905a52b0f48efa64cb63b5f9898
JhandySelencio <154280174+JhandySelencio@users.noreply.github.com>
2026-08-27 17:43:10 +0800

    Updated as of 8/27/2026

---
+++ Services/EmailSender.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+
+namespace SmartScan.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _config;
+
+        public EmailSender(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage) 
+        {
+            var emailSettings = _config.GetSection("EmailSettings");
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(emailSettings["SenderName"], emailSettings["SenderEmail"]));
+            message.To.Add(MailboxAddress.Parse(email));
+            message.Subject = subject;
+
+            var bodyBuilder = new BodyBuilder
+            {
+                HtmlBody = htmlMessage
+            };
+            message.Body = bodyBuilder.ToMessageBody();
+
+            using var client = new SmtpClient();
+
+            // Connecting to port 587 for STARTTLS
+            await client.ConnectAsync(
+                emailSettings["SmtpServer"],
+                int.Parse(emailSettings["Port"]!),
+                SecureSocketOptions.StartTls
+            );
+
+            // Aunthenticate with your sender email & app password
+            await client.AuthenticateAsync(emailSettings["SenderEmail"], emailSettings["Password"]);
+
+            // Sends the email
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+        }
+    }
+}
--- obj/Debug/net9.0/SmartScan.AssemblyInfo.cs
+++ obj/Debug/net9.0/SmartScan.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartScan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd13ca16cc4feb030e2ee12a694e95bbbffe7f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c012a062788804360966ce62f7cc5409c1cdfe1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartScan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartScan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
